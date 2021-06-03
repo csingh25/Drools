92375b107032e5df2684a00e3f89183e30256ecc
csingh25 <54691277+csingh25@users.noreply.github.com>
2021-06-03 14:00:16 -0400

    Update UWService.java

--- src/main/java/com/drools/service/UWService.java
+++ src/main/java/com/drools/service/UWService.java
@@ -1,11 +1,14 @@
 package com.drools.service;
 
-import org.kie.api.runtime.KieContainer;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.kie.api.event.rule.AfterMatchFiredEvent;
+import org.kie.api.event.rule.DefaultAgendaEventListener;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.drools.model.LifePlanCase;
 import com.drools.model.SingleLifePlanCase;
 
 @Service
@@ -14,13 +17,35 @@ public class UWService {
 	
 	@Autowired
 	private KieSession session;
+	private static Logger log =  Logger.getLogger(UWService.class);
 
 	public SingleLifePlanCase getProductPremium(SingleLifePlanCase product) {
 		//get the stateful session
 		//KieSession kieSession = kieContainer.newKieSession("rulesSession");
-		session.insert(product);
-		session.fireAllRules();
-		session.dispose();
+		try {
+			
+			session.addEventListener(new DefaultAgendaEventListener() {
+                 
+	                //this event will be executed after the rule matches with the model data
+				 	
+	                public void afterMatchFired(AfterMatchFiredEvent event) {
+	                    super.afterMatchFired(event);
+	                    log.info(event.getMatch().getRule().getName());///prints the rule name that fires the event 
+	                }
+	                
+	            });
+			session.insert(product);
+			session.getAgenda().getAgendaGroup(product.getName()).setFocus();
+			session.fireAllRules();
+			session.dispose();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return product;
 	}
+	
+	
 }
+
